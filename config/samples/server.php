8999e58f15995f54e4b37ad2a69cce955e18fe5a
ruesin <ruesin@gmail.com>
2019-07-22 22:24:03 +0800

    improve

--- config/samples/server.php
+++ config/samples/server.php
@@ -2,43 +2,23 @@
 return [
     'process' => [
         'server_type' => 'process',
-        'daemonize' => true,
+        'daemonize' => false,
         'process_name' => 'dq_process',
         'worker_num' => 2,
-        'max_request' => 0,
-        'log_file' => dirname(__DIR__).'/runtime/swoole.log',
-        'entrance' => '\App\Process::run',
+        'max_request' => 10000,
+        'log_file' => dirname(__DIR__) . '/runtime/swoole.log',
+        'entrance' => '\App\App::process',
     ],
 
     'http' => [
         'server_type' => 'http',
         'daemonize' => false,
         'process_name' => 'dq_http',
-        'host' => '127.0.0.1',
+        'host' => '127.0.0.1', //'0.0.0.0',
         'port' => '9501',
         'worker_num' => 2,
-        'task_worker_num' => 1,
         'max_request' => 10000,
-        'log_file' => dirname(__DIR__).'/runtime/swoole_http.log',
-        'entrance'    => '\App\App::http',
-        'async'    => false,
-        'signature'   => '\App\Utils\Sign::verify',
-        'trace_log'   => true
-    ],
-
-    'tcp' => [
-        'server_type' => 'tcp',
-        'daemonize' => false,
-        'process_name' => 'dq_tcp',
-        'host' => '127.0.0.1',
-        'port' => '9502',
-        'worker_num' => 2,
-        'task_worker_num' => 1,
-        'max_request' => 10000,
-        'log_file' => dirname(__DIR__).'/runtime/swoole_tcp.log',
-        'entrance'    => '\App\App::http',
-        'async'    => false,
-        'signature'   => '\App\Utils\Sign::verify',
-        'trace_log'   => true
-    ],
+        'log_file' => dirname(__DIR__) . '/runtime/swoole_http.log',
+        'entrance' => '\App\App::http',
+    ]
 ];
