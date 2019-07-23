870c6d843cc2b07a4b666b2aa61173cb23e406c6
ruesin <ruesin@gmail.com>
2019-07-23 11:03:42 +0800

    delete info twice

--- app/Queue/Drop.php
+++ app/Queue/Drop.php
@@ -7,19 +7,25 @@ class Drop extends Base
 {
     public function handle()
     {
+        //删除消息体
+        $this->defaultInstance->del([Queue::messageName($this->queue_name)]);
+        //删除延时消息
+        Queue::getDelayInstance()->del([Queue::delayName($this->queue_name)]);
+        //删除已读消息
+        Queue::getReadInstance()->del([Queue::readName($this->queue_name)]);
+        //删除活跃消息
+        Queue::getActiveInstance()->del([Queue::activeName($this->queue_name)]);
+
         //删除队列定义
         $this->defaultInstance->hdel(Queue::queueInfoName(), $this->queue_name);
         $this->defaultInstance->lrem(Queue::queueListName(), 0, $this->queue_name);
 
         //删除消息体
         $this->defaultInstance->del([Queue::messageName($this->queue_name)]);
-
         //删除延时消息
         Queue::getDelayInstance()->del([Queue::delayName($this->queue_name)]);
-
         //删除已读消息
         Queue::getReadInstance()->del([Queue::readName($this->queue_name)]);
-
         //删除活跃消息
         Queue::getActiveInstance()->del([Queue::activeName($this->queue_name)]);
 
