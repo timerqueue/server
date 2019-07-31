ba8c70c0cc148a2d7f2a83895e861f74f869ebe0
ruesin <ruesin@gmail.com>
2019-07-31 20:52:59 +0800

    truncate

--- app/Queue/Truncate.php
+++ app/Queue/Truncate.php
@@ -2,12 +2,26 @@
 namespace App\Queue;
 
 use App\Utils\Queue;
-use Ruesin\Utils\Redis;
 
+/**
+ * 清空队列
+ */
 class Truncate extends Base
 {
     public function handle()
     {
+        for ($i = 0; $i < 5; $i++) {
+            if (!Queue::lock($this->queue_name))
+                usleep(50000);
+        }
 
+        $this->connection->transaction()
+            ->del([
+                $this->messageName,
+                $this->delayName,
+                $this->readName,
+                $this->activeName
+            ]);
+        return self::response(200, [], 'Queue truncate successfully!');
     }
 }
